e5d05c6566a5a5ee8774a46528f2a8ed811e6b6e
Diogenes de Carvalho Pereira <diogenes.pereira@luizalabs.com>
2021-08-04 21:39:50 -0300

    doc: SwaggerUI

--- src/main/java/br/com/digitalhouse/apiecommerce/config/SecurityConfiguration.java
+++ src/main/java/br/com/digitalhouse/apiecommerce/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static br.com.digitalhouse.apiecommerce.constants.SecurityConstants.DOC_URL;
 import static br.com.digitalhouse.apiecommerce.constants.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
@@ -23,6 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(DOC_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new AuthenticationFilter(authenticationManager()))
---
+++ src/main/java/br/com/digitalhouse/apiecommerce/config/SpringFoxConfig.java
@@ -0,0 +1,65 @@
+package br.com.digitalhouse.apiecommerce.config;
+
+import org.assertj.core.util.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+@Configuration
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+    private final Logger log = LoggerFactory.getLogger(SpringFoxConfig.class);
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .apiInfo(ApiInfo.DEFAULT)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(true);
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+}
